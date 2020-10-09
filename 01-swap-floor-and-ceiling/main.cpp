182c884ed579a2017aa2fbc8cfe38a9bdea94433
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2020-10-09 22:52:22 +0300

    Implement lab 01

---
+++ 01-swap-floor-and-ceiling/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+    double a;
+
+    std::cout << "Введіть число виду ccc.ddd: ";
+    scanf("%lf", &a);
+
+    int ccc = (int) a;
+    int ddd = (int) round( (a - ccc) * pow(10, 3) );
+
+    double result = ddd + ccc * pow(10, -3);
+    printf("%lg", result);
+
+    return 0;
+}
