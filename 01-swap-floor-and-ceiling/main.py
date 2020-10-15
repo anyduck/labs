7e2e1924c1d03491983def43dc3a262e161f5101
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2020-10-15 12:39:37 +0300

    [lab 01] Add comments

--- 01-swap-floor-and-ceiling/main.py
+++ 01-swap-floor-and-ceiling/main.py
@@ -1,13 +1,13 @@
 #! /usr/bin/python
 
 def main():
-    a = float(input("Введіть число виду ccc.ddd: "))
+    a = float(input("Введіть число виду ccc.ddd: "))  # Вхіде число виду ccc.ddd
 
-    ccc = int(a)
-    ddd = round((a - ccc) * 10**3)
+    ccc = int(a)                    # Ціла частина вхідного числа
+    ddd = round((a - ccc) * 10**3)  # Дробова частина вхідного чила
 
-    result = ddd + ccc * 10**-3  # ddd.ccc
-    print(result)
+    result = ddd + ccc * 10**-3     # Кінцеве число виду ddd.ccc
+    print('Кінцеве число виду ddd.ccc:', result)
 
 
 if __name__ == "__main__":
