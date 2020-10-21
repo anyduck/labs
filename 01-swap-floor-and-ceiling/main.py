85367ab1297611a14c97ca52b2dcb1376264b0a8
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2020-10-21 10:55:30 +0300

    [lab 01] Rewrite for new variant 27

--- 01-swap-floor-and-ceiling/main.py
+++ 01-swap-floor-and-ceiling/main.py
@@ -1,13 +1,12 @@
 #! /usr/bin/python
 
 def main():
-    a = float(input("Введіть число виду ccc.ddd: "))  # Вхіде число виду ccc.ddd
+    leg_a = float(input("Введіть катет (a): "))            # Катет a
+    hypotenuse = float(input("Введіть гіпотенузу (c): "))  # Гіпотенуза
 
-    ccc = int(a)                    # Ціла частина вхідного числа
-    ddd = round((a - ccc) * 10**3)  # Дробова частина вхідного чила
-
-    result = ddd + ccc * 10**-3     # Кінцеве число виду ddd.ccc
-    print('Кінцеве число виду ddd.ccc:', result)
+    leg_b = (hypotenuse**2 - leg_a**2) ** 0.5  # Катет b за теоремою піфагора
+    radius = (leg_a + leg_b - hypotenuse) / 2  # Радіус вписаного кола
+    print('Радіус вписаного кола (r):', radius)
 
 
 if __name__ == "__main__":
