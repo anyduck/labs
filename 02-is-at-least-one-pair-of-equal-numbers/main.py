c39d1c11f0c92d167b4294b4676229acc32ce06c
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2020-10-28 11:25:07 +0200

    [lab02] Rewrite for asd alhorytm

--- 02-is-at-least-one-pair-of-equal-numbers/main.py
+++ 02-is-at-least-one-pair-of-equal-numbers/main.py
@@ -6,7 +6,14 @@ def main():
     c = float(input("Введіть число c: "))
 
     # Чи є хоча б одна пара чисел рівних між собою
-    is_equal = (a == b) or (a == c) or (b == c)
+    if a == b:
+        is_equal = True
+    elif a == c:
+        is_equal = True
+    elif b == c:
+        is_equal = True
+    else:
+        is_equal = False
 
     result = "є хоча б одна пара" if is_equal else "немає хоча б однієї пари"
     print("Серед цих чисел", result, "рівних між собою")
