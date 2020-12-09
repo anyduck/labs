35379fc70306cc031c2a0bc2ffe9c16d8108ea57
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2020-12-09 11:28:50 +0200

    [lab06] Fix: add exchange function

--- 06-gcd-of-three-numbers/cpp/main.cpp
+++ 06-gcd-of-three-numbers/cpp/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void init(int &, int &, int &);   // введення даних
 int gcd(int, int, int);           // обчислення найбільшого спільного дільника
+template <typename T> void exchange(T &, T &); // обмін двох змінних
 void browse(int, int, int, int);  // вивдення результатів
 
 int main() {
@@ -23,25 +24,36 @@ void init(int &a, int &b, int &c) {
     cout << "Введіть c: "; cin >> c;
 }
 
+// Знаходить найбільше спільне кратне ТРЬОХ чисел.
+// Використувує власитвість НСД(a, b, c) = НСД(НСД(a, b), c)
+inline int gcd(int a, int b, int c) {
+    int gcd(int, int);
+
+    return gcd(gcd(a, b), c);
+}
+
+template <typename T>
+void exchange(T &a, T &b) {
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
 // Знаходить найбільше спільне кратне ДВОХ чисел.
 // Використовує алгоритм Евкліда.
 int gcd(int a, int b) {
     if (a < b)
-        swap(a, b);
+        exchange(a, b);
 
     while (b != 0) {
         a %= b;
-        swap(a, b);
+        exchange(a, b);
     }
 
     return abs(a);
 }
 
-// Знаходить найбільше спільне кратне ТРЬОХ чисел.
-// Використувує власитвість НСД(a, b, c) = НСД(НСД(a, b), c)
-inline int gcd(int a, int b, int c) {
-    return gcd(gcd(a, b), c);
-}
 
 void browse(int a, int b, int c, int result) {
     cout << "НСД(" << a << ", " << b << ", " << c << ") = " << result << endl;
